5e4a3797c5f2b82e8dcfaf1e48d8786f1cee262f
Kunlanit (Fern) <kunlanit@micware-ap.com>
2025-07-21 14:58:01 +0700

    [ADD] Add project file

---
+++ app/src/main/java/com/example/attendanceapp/data/model/ActivitiesLogCommonItem.kt
@@ -0,0 +1,7 @@
+package com.example.attendanceapp.data.model
+
+data class ActivitiesLogCommonItem(
+    val dateRequest : String,
+    val requestType : String,
+    val details : String
+)
---
+++ app/src/main/java/com/example/attendanceapp/data/model/CheckOutData.kt
@@ -0,0 +1,7 @@
+package com.example.attendanceapp.data.model
+
+data class CheckOutData(
+    val dateCheckOut : String,
+    val requestType : String,
+    val checkOutTime : String
+)
