bfb9512d19e13c60199ab21a0c3ad898fec44c1c
Kunlanit (Fern) <kunlanit@micware-ap.com>
2025-07-29 18:07:55 +0700

    [ADD] Add Retrofit, Gson, OkHttp Logging Interceptor, and Coroutines Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
 dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
