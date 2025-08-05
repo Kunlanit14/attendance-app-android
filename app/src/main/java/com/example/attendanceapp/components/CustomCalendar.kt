65c470250ce3a968d49efd71fa3938aff3035e70
Kunlanit (Fern) <kunlanit@micware-ap.com>
2025-08-05 16:54:00 +0700

    [ADD] Create Custom Calendar activity and modify UI (app bar) layout.

---
+++ app/src/main/java/com/example/attendanceapp/components/CustomCalendar.kt
@@ -0,0 +1,21 @@
+package com.example.attendanceapp.components
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.attendanceapp.R
+
+class CustomCalendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_custom_calendar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
